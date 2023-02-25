b16ba55f54ec3b95db54e6e9008fcd2fffc191c6
easyhe <euneundh@gmail.com>
2023-02-25 20:27:53 +0900

    package.json dependency 처리

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { applyMiddleware, compose, createStore } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -8,12 +8,8 @@ import theme from 'theme';
 import './index.css';
 import App from './App';
 import rootReducer from 'store/modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import logger from 'redux-logger';
 
-const enhancer =
-  process.env.NODE_ENV === 'production' ? compose(applyMiddleware()) : composeWithDevTools(applyMiddleware(logger));
-const store = createStore(rootReducer, enhancer);
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
